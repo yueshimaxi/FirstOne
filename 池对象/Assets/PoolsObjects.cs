d2e3f7b47c5ffc8c0bf044176be43eba3711c3c7
PS-03\Admin <1071556447@qq.com>
2019-07-11 17:22:20 +0800

    update

---
+++ 池对象/Assets/PoolsObjects.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolsObjects : MonoBehaviour {
+    public GameObject poolObject;
+
+    public int poolSize=5;
+
+    public List<GameObject > poolObjectsList = new List<GameObject>();
+
+    public Transform spawnPool;
+
+    public GameObject   GetObject()
+    {
+        for (int i = 0; i < poolObjectsList.Count; i++)
+         {
+            if (!poolObjectsList [i].activeInHierarchy)
+            {
+                poolObjectsList[i].SetActive(true);
+
+                poolObjectsList[i].GetComponent<Rigidbody>().velocity =transform .forward * 5;
+                return poolObjectsList[i];
+
+            }
+        }
+        return createPoolObject(poolObject, spawnPool);
+    }
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < poolSize; i++)
+        {
+         GameObject go=   createPoolObject(poolObject, spawnPool);
+
+            go.SetActive(false);
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input .GetKeyDown (KeyCode .J))
+        {
+            GetObject();
+        }
+	}
+
+  public GameObject    createPoolObject(GameObject Poolobject,Transform spawn)
+    {
+        GameObject go = GameObject.Instantiate(Poolobject);
+
+        go.transform.parent = spawn;
+        go.transform.position = spawn.position;
+        go.transform.rotation = spawn.rotation;
+        go.transform.localScale = new Vector3(1, 1, 1);
+
+        go .GetComponent<Rigidbody>().velocity = transform.forward * 5;
+
+        poolObjectsList.Add(go);
+
+        return go;
+    }
+    
+}
---
+++ 池对象/Assets/SetactiveByTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetactiveByTime : MonoBehaviour {
+    public float timer=0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer >= 5)
+        {
+            this.gameObject.SetActive(false);
+            timer = 0;
+            transform.localPosition = Vector3.zero;
+        }
+	}
+}
